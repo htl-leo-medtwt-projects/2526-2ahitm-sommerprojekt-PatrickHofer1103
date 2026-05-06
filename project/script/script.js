d153693d2b811ebd0dc6ef3c94e765ea84b28606
Patrick Hofer <p.hofer@students.htl-leonding.ac.at>
2026-05-06 15:55:40 +0200

    Add GSAP animation for settings popup and include GSAP library in HTML

--- project/script/script.js
+++ project/script/script.js
@@ -31,6 +31,14 @@ function settings() {
       </div>
     </div>
   `;
+  gsap.fromTo("#settings", {
+    opacity: 0,
+    scale: 0.5
+  }, {
+    opacity: 1,
+    scale: 1,
+    duration: 0.5
+  });
 }
 
 function settingsSchließen() {
