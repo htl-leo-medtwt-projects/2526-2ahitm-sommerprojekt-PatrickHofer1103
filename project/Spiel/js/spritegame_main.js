2381648a7ba6059238f93dc2082480d5e72a72c2
Patrick Hofer <p.hofer@students.htl-leonding.ac.at>
2026-05-06 18:46:33 +0200

    Implement settings menu with GSAP animations and update game HTML structure

--- project/Spiel/js/spritegame_main.js
+++ project/Spiel/js/spritegame_main.js
@@ -24,3 +24,36 @@ function updateHUD(){
     GAME_SCREEN.debug_output.innerHTML = `x: ${PLAYER.box.style.left} | y: ${PLAYER.box.style.top} | animation: ${PLAYER.spriteImgNumber} | count: ${PLAYER.coinCount}`;
 }
 
+function settings() {
+  document.getElementById("settings").style.display = "flex";
+  document.getElementById("settings").innerHTML =`
+    <div id="SettingsContent">
+      <div id="CloseSettings" onclick="settingsSchließen()">x</div>
+      <div id="SettingsHeader">
+        <h2>Settings</h2>
+      </div>
+      <div id="SettingsBody">
+        <div class="SettingsGroup">
+          <p>Audio Settings</p>
+        </div>
+        <button class="SettingsButton">Stumm</button>
+        <button class="SettingsButton">Laut</button>
+        <a class="SettingsButton" href="../index.html">Back</a>
+      </div>
+    </div>
+  `;
+  gsap.fromTo("#settings", {
+    opacity: 0,
+    scale: 0.5
+  }, {
+    opacity: 1,
+    scale: 1,
+    duration: 0.5
+  });
+}
+
+function settingsSchließen() {
+  document.getElementById("settings").innerHTML =``;
+  document.getElementById("settings").style.display = "none";
+}
+
